eab8a8e8f2395bcf4612a70982a15197507987d6
mvglasow <michael -at- vonglasow.com>
2014-07-25 21:49:32 +0200

    Temporarily disable all CellInfo-related processing
    Signed-off-by: mvglasow <michael -at- vonglasow.com>

--- src/com/vonglasow/michael/satstat/MainActivity.java
+++ src/com/vonglasow/michael/satstat/MainActivity.java
@@ -491,6 +491,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 	 * The {@link PhoneStateListener} for getting radio network updates 
 	 */
 	private final PhoneStateListener mPhoneStateListener = new PhoneStateListener() {
+		/* TODO: disabled for debugging
 		// Requires API level 17. Many phones don't implement this method at all and will return null,
 		// the ones that do implement it return only certain cell types (none that we support at this point).
 		@TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
@@ -504,6 +505,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 			}
 			showCells();
 	 	}
+	 	*/
 	 	
 		public void onCellLocationChanged (CellLocation location) {
 			mCellsGsm.removeSource(CellTower.SOURCE_CELL_LOCATION);
@@ -2218,6 +2220,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 				mCellsGsm.updateAll(networkOperator, neighboringCells);
 			}
 			
+			/* TODO: disabled for debugging
 			// Requires API level 17. Many phones don't implement this method at all and will return null,
 			// the ones that do implement it return only certain cell types (none that we support at this point).
 			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
@@ -2228,6 +2231,7 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 					mCellsLte.updateAll(allCells);
 				}
 			}
+			*/
 			
 			showCells();
 
