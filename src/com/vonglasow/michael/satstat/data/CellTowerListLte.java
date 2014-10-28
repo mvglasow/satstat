2cb27888877f87ae1349a90e2ca327fd97af2694
mvglasow <michael -at- vonglasow.com>
2014-10-28 22:49:53 +0100

    RIL: allow adding CellLocation or NeighboringCellInfo to CellTowerListLte
    Signed-off-by: mvglasow <michael -at- vonglasow.com>

--- src/com/vonglasow/michael/satstat/data/CellTowerListLte.java
+++ src/com/vonglasow/michael/satstat/data/CellTowerListLte.java
@@ -7,6 +7,10 @@ import android.os.Build;
 import android.telephony.CellIdentityLte;
 import android.telephony.CellInfo;
 import android.telephony.CellInfoLte;
+import android.telephony.NeighboringCellInfo;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
+import android.util.Log;
 
 public class CellTowerListLte extends CellTowerList<CellTowerLte> {
 	/**
@@ -29,6 +33,99 @@ public class CellTowerListLte extends CellTowerList<CellTowerLte> {
 			return null;
 		else
 			return this.get(entry);
+	}
+	
+	/**
+	 * Adds or updates a cell tower.
+	 * <p>
+	 * If the cell tower is already in the list, its data is updated; if not, a
+	 * new entry is created.
+	 * <p>
+	 * This method will set the cell's identity data. After this call,
+	 * {@link #isServing()} will return {@code true} for this cell.
+	 * @param networkOperator The network operator, as returned by {@link android.telephony.TelephonyManager#getNetworkOperator()}.
+	 * @param location The {@link android.telephony.GsmCellLocation}, as returned by {@link android.telephony.TelephonyManager#getCellLocation()}.
+	 * @return The new or updated entry.
+	 */
+	public CellTowerLte update(String networkOperator, GsmCellLocation location) {
+		int mcc = CellTower.UNKNOWN;
+		int mnc = CellTower.UNKNOWN;
+		if (networkOperator.length() > 3) {
+			mcc = Integer.parseInt(networkOperator.substring(0, 3));
+			mnc = Integer.parseInt(networkOperator.substring(3));
+		}
+		CellTowerLte result = this.get(mcc, mnc, location.getLac(), location.getCid());
+		if (result == null)
+			result = this.get(location.getPsc());
+		if (result == null)
+			result = new CellTowerLte(mcc, mnc, location.getLac(), location.getCid(), location.getPsc());
+		if (result.getMcc() == CellTower.UNKNOWN)
+			result.setMcc(mcc);
+		if (result.getMnc() == CellTower.UNKNOWN)
+			result.setMnc(mnc);
+		if (result.getTac() == CellTower.UNKNOWN)
+			result.setTac(location.getLac());
+		if (result.getCi() == CellTower.UNKNOWN)
+			result.setCi(location.getCid());
+		if (result.getPci() == CellTower.UNKNOWN)
+			result.setPci(location.getPsc());
+		this.put(result.getText(), result);
+		this.put(result.getAltText(), result);
+		result.setCellLocation(true);
+		Log.d(this.getClass().getName(), String.format("Added GsmCellLocation for %s", result.getText()));
+		return result;
+	}
+	
+	/**
+	 * Adds or updates a cell tower.
+	 * <p>
+	 * If the cell tower is already in the list, its data is updated; if not, a
+	 * new entry is created. Cells whose network type is not LTE will be
+	 * rejected.
+	 * <p>
+	 * This method will set the cell's identity data, generation and its signal
+	 * strength.
+	 * @return The new or updated entry, or {@code null} if the cell was rejected
+	 */
+	public CellTowerLte update(String networkOperator, NeighboringCellInfo cell) {
+		int mcc = CellTower.UNKNOWN;
+		int mnc = CellTower.UNKNOWN;
+		if (networkOperator.length() > 3) {
+			mcc = Integer.parseInt(networkOperator.substring(0, 3));
+			mnc = Integer.parseInt(networkOperator.substring(3));
+		}
+		CellTowerLte result = this.get(mcc, mnc, cell.getLac(), cell.getCid());
+
+		if (result == null)
+			result = this.get(cell.getPsc());
+		if (result == null)
+			result = new CellTowerLte(mcc, mnc, cell.getLac(), cell.getCid(), cell.getPsc());
+		result.setNeighboringCellInfo(true);
+		int networkType = cell.getNetworkType();
+		switch (networkType) {
+		case TelephonyManager.NETWORK_TYPE_LTE:
+				//result.setAsu(cell.getRssi());
+				//FIXME: if we ever get LTE cells this way, how can we find out their signal strength?
+				break;
+			default:
+				// not an LTE cell, return
+				return null;
+		}
+		result.setNetworkType(networkType);
+		if (result.getMcc() == CellTower.UNKNOWN)
+			result.setMcc(mcc);
+		if (result.getMnc() == CellTower.UNKNOWN)
+			result.setMnc(mnc);
+		if (result.getTac() == CellTower.UNKNOWN)
+			result.setTac(cell.getLac());
+		if (result.getCi() == CellTower.UNKNOWN)
+			result.setCi(cell.getCid());
+		if (result.getPci() == CellTower.UNKNOWN)
+			result.setPci(cell.getPsc());
+		this.put(result.getText(), result);
+		this.put(result.getAltText(), result);
+		Log.d(this.getClass().getName(), String.format("Added NeighboringCellInfo for %s", result.getText()));
+		return result;
 	}
 	
 	/**
@@ -88,5 +185,20 @@ public class CellTowerListLte extends CellTowerList<CellTowerLte> {
 		for (CellInfo cell : cells)
 			if (cell instanceof CellInfoLte)
 				this.update((CellInfoLte) cell);
+	}
+	
+	/**
+	 * Adds or updates a list of cell towers.
+	 * <p>
+	 * This method first calls {@link #removeSource(int)} with
+	 * {@link com.vonglasow.michael.satstat.data.CellTower#SOURCE_NEIGHBORING_CELL_INFO}
+	 * as its argument. Then it iterates through all entries in {@code cells}
+	 * and updates each entry by calling {@link #update(NeighboringCellInfo)},
+	 * passing that entry as the argument.
+	 */
+	public void updateAll(String networkOperator, List<NeighboringCellInfo> cells) {
+		this.removeSource(CellTower.SOURCE_NEIGHBORING_CELL_INFO);
+		for (NeighboringCellInfo cell : cells)
+			this.update(networkOperator, cell);
 	}
 }
